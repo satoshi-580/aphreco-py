5ba5b9ccf01595278a9deae1c513d54f457eb869
sts-koyama <koyamasatoshi.3@gmail.com>
2022-01-23 20:08:15 +0900

    add Optimizer

--- src/aphreco/solve/optimize.py
+++
@@ -1,8 +0,0 @@
-class OptOptions:
-    def __init__(self):
-        pass
-
-
-class Optimizer:
-    def __init__(self, options: OptOptions = OptOptions()):
-        self.options = options
---
+++ src/aphreco/solve/optimize/base.py
@@ -0,0 +1,38 @@
+import abc
+
+
+def set_option(options, key, value):
+    if key in options.keys():
+        check_option_type(options[key], value)
+        options[key] = value
+    else:
+        raise KeyError(f"invalid option key: {key}")
+    return options
+
+
+def check_option_type(old_value, new_value):
+    if type(old_value) != type(new_value):
+        raise TypeError(
+            f"invalid option value: {new_value}\nexpected {type(old_value)}"
+        )
+
+
+class BaseOptimizeMethod(abc.ABC):
+    @property
+    def is_default(self):
+        return self._is_default
+
+    @is_default.setter
+    def is_default(self, value):
+        self._is_default = value
+
+    @abc.abstractmethod
+    def set_options(self, **options):
+        if options:
+            self.is_default = False
+            for key, value in options.items():
+                self.options = set_option(self.options, key, value)
+
+    @abc.abstractmethod
+    def collect_options(self):
+        return str(self.options)
