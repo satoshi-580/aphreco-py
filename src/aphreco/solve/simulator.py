92584e76d31b330aaeae35c666160900ab8e11c9
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-07 17:03:10 +0900

    remove unsed import

--- src/aphreco/solve/simulator.py
+++ src/aphreco/solve/simulator.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 
 from aphreco.core import Model
-from aphreco.enums import ItemType
 
 from .format import SimFormatter
 from .simulate.base import BaseStepMethod
