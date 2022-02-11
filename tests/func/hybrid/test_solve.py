490e8b96f59d55953180e2e647c3400dddbdfb8e
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-11 16:56:18 +0900

    test smptime formats

--- tests/func/hybrid/test_solve.py
+++ tests/func/hybrid/test_solve.py
@@ -4,12 +4,23 @@ import pytest
 
 @pytest.mark.hybrid
 class TestTypicalSolve:
-    def test_outtime_format(self, phase1):
+    def test_smptime_format(self, phase1):
         simulator = ap.Simulator()
-        start = 0
-        stop = 60
-        step = 0.1
+
+        # tuple of (start, stop, step)
+        start, stop, step = (0, 0.05, 0.01)
         simres = simulator.run(phase1, (start, stop, step))
+        assert simres.t == [0.0, 0.01, 0.02, 0.03, 0.04]
+
+        # list of [timepoints]
+        smptimes = [0.0, 0.1, 0.2, 0.5, 1.0, 2.25, 5.0]
+        simres = simulator.run(phase1, smptimes)
+        assert simres.t == smptimes
+
+        # list of [timepoints]
+        smptimes = [0.0, 0.1, 0.2, 0.5, 1.0, 2.25]
+        simres = simulator.run(phase1, smptimes)
+
         assert True
 
     def test_simulate(self, phase1):
