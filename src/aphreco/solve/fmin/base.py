a724f0d81027f1ffcb8609b40037e88ce1a54be0
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-12 13:30:36 +0900

    integrate set_options

--- src/aphreco/solve/fmin/base.py
+++ src/aphreco/solve/fmin/base.py
@@ -1,24 +1,9 @@
 import abc
-from typing import List, Union
+
+from aphreco.solve.utils import set_option
 
 
-def set_option(options, key, value):
-    if key in options.keys():
-        check_option_type(options[key], value)
-        options[key] = value
-    else:
-        raise KeyError(f"invalid option key: {key}")
-    return options
-
-
-def check_option_type(old_value, new_value):
-    if type(old_value) != type(new_value):
-        raise TypeError(
-            f"invalid option value: {new_value}\nexpected {type(old_value)}"
-        )
-
-
-class BaseOptimizeMethod(abc.ABC):
+class BaseFminAlgorithm(abc.ABC):
     @property
     def name(self):
         return self._name
@@ -39,18 +24,3 @@ class BaseOptimizeMethod(abc.ABC):
 
     def collect_options(self):
         return str(self.options)
-
-
-class Optimizer:
-    def __init__(
-        self,
-        methods: Union[None, BaseOptimizeMethod, List[BaseOptimizeMethod]] = None,
-    ):
-        if methods is None:
-            self.methods = methods
-        elif isinstance(methods, list):
-            self.methods = methods
-        elif isinstance(methods, BaseOptimizeMethod):
-            self.methods = [methods]
-        else:
-            raise TypeError("invalid methods type")
--- src/aphreco/solve/step/base.py
+++ src/aphreco/solve/step/base.py
@@ -1,20 +1,6 @@
 import abc
 
-
-def check_option_type(old_value, new_value):
-    if type(old_value) != type(new_value):
-        raise TypeError(
-            f"invalid option value: {new_value}\nexpected {type(old_value)}"
-        )
-
-
-def set_option(options, key, value):
-    if key in options.keys():
-        check_option_type(options[key], value)
-        options[key] = value
-    else:
-        raise KeyError(f"invalid option key: {key}")
-    return options
+from aphreco.solve.utils import set_option
 
 
 class BaseStepMethod(abc.ABC):
