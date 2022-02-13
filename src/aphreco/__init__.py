30e053ca59d875bdf80d5a34d5ec5514441ec63c
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-13 19:09:02 +0900

    fix bug

--- src/aphreco/__init__.py
+++ src/aphreco/__init__.py
@@ -1,8 +1,8 @@
 from aphreco import errors
 from aphreco.core import A, Con, Model, P, Reg, Variable, X, Y
+from aphreco.data import read_data
 from aphreco.enums import ItemType
 from aphreco.solve import Optimizer, Simulator, fmin, step
-from aphreco.src.aphreco.data import read_data
 
 # from .solve import Optimizer, Simulator, optimize, simulate
 # from .unit import Unit
