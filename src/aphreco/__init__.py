c3d415bb0f1623c31c145d8c8156624bf114335f
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-13 14:16:42 +0900

    enable to read observation csv by aphreco.read_data()

--- src/aphreco/__init__.py
+++ src/aphreco/__init__.py
@@ -2,6 +2,7 @@ from aphreco import errors
 from aphreco.core import A, Con, Model, P, Reg, Variable, X, Y
 from aphreco.enums import ItemType
 from aphreco.solve import Optimizer, Simulator, fmin, step
+from aphreco.src.aphreco.data import read_data
 
 # from .solve import Optimizer, Simulator, optimize, simulate
 # from .unit import Unit
