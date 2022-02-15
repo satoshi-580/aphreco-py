7c42654989537df01394c218de9e7e0e89c88535
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-15 22:46:00 +0900

    reorder

--- src/aphreco/types.py
+++ src/aphreco/types.py
@@ -2,8 +2,8 @@ from typing import Any, Dict, List, Optional, Tuple
 
 from aphreco.enums import ItemType
 
-Table = List[Tuple[str, float, float, Optional[float], Optional[float], int]]
 NamesDict = Dict[str, Tuple[ItemType, int]]
 ValsDict = Dict[str, float]
 TermsDict = Dict[str, Dict[Any, str]]
 UnksDict = Dict[str, Tuple[float, int, Optional[Tuple[float, float]]]]
+Table = List[Tuple[str, float, float, Optional[float], Optional[float], int]]
