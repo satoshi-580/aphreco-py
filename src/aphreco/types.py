e5598f09d94519629f3681727c30c6062179c1c3
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-16 20:46:49 +0900

    add explanations

--- src/aphreco/types.py
+++ src/aphreco/types.py
@@ -2,8 +2,21 @@ from typing import Any, Dict, List, Optional, Tuple
 
 from aphreco.enums import ItemType
 
+# {yp_name: (itemtype, yp_index)}
 NamesDict = Dict[str, Tuple[ItemType, int]]
+
+# {yp_name: value}
 ValsDict = Dict[str, float]
+
+# {
+#   "ode": {yname: rhs}
+#   "rec": {(start, stop, step): rhs}
+#   "cre": {yname: rhs}
+# }
 TermsDict = Dict[str, Dict[Any, str]]
+
+# {x_name: (x0, p_index, (lb, ub))}
 UnksDict = Dict[str, Tuple[float, int, Optional[Tuple[float, float]]]]
+
+# [(y_name, t, y, terr, yerr, y_index)]
 Table = List[Tuple[str, float, float, Optional[float], Optional[float], int]]
