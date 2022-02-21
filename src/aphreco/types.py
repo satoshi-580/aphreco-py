fec52e2083348635ca0d4debbb2beb486277fef9
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-21 15:44:27 +0900

    start conditioned terms

--- src/aphreco/types.py
+++ src/aphreco/types.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from aphreco.enums import ItemType
 
@@ -10,10 +10,10 @@ ValsDict = Dict[str, float]
 
 # {
 #   "ode": {yname: rhs}
-#   "rec": {(start, stop, step): rhs}
+#   "rec": {(start, stop, step): rhs or {cond: (rhs of truecase, rhs of falsecase)}}
 #   "cre": {yname: rhs}
 # }
-TermsDict = Dict[str, Dict[Any, str]]
+TermsDict = Dict[str, Dict[Any, Union[str, Dict[str, Tuple[str, str]]]]]
 
 # {x_name: (x0, p_index, (lb, ub))}
 UnksDict = Dict[str, Tuple[float, int, Optional[Tuple[float, float]]]]
