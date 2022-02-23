dcd33d1b497c0c46e372080a1d20f38b4a93f922
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-23 20:34:52 +0900

    enable to use ap.Str

--- src/aphreco/__init__.py
+++ src/aphreco/__init__.py
@@ -1,10 +1,7 @@
 from aphreco import errors
-from aphreco.core import A, Con, Model, P, Reg, Variable, X, Y
+from aphreco.core import A, Con, Model, P, Reg, Str, Variable, X, Y
 from aphreco.core.data import read_data
 from aphreco.enums import ItemType
 from aphreco.solve import Optimizer, Simulator, fmin, step
-
-# from .solve import Optimizer, Simulator, optimize, simulate
-# from .unit import Unit
 
 __version__ = "0.1.0"
