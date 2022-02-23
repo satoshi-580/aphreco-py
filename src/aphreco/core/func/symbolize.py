34e92941de4104112d5b7330cf50e1137e4cea8e
sts-koyama <koyamasatoshi.3@gmail.com>
2022-02-23 14:14:09 +0900

    enable to get symset not only from true/false but also from cond

--- src/aphreco/core/func/symbolize.py
+++ src/aphreco/core/func/symbolize.py
@@ -1,34 +1,38 @@
-from typing import List, Set, Union
+import re
+from typing import Set, Union
 
 import sympy
 from aphreco.types import Ternary
 
 
+def _get_atoms(term):
+    return sympy.sympify(term).atoms(sympy.Symbol)
+
+
+PATTERN = re.compile(r"==|!=|>=|<=|<|>|&&|\|")
+
+
+def _symset_in_condition(cond: str) -> Set[str]:
+    cond_parts = PATTERN.split(cond)
+    symset: Set[str] = set()
+    for part in cond_parts:
+        symset = symset | _get_atoms(part)
+    return symset
+
+
 def extract_symset(term: Union[str, Ternary]) -> Set[str]:
     if isinstance(term, str):
-        symbols_set = sympy.sympify(term).atoms(sympy.Symbol)
+        symbols_set = _get_atoms(term)
         str_symbols_set = {str(symbol) for symbol in symbols_set}
 
     elif isinstance(term, tuple):
-        # symbols_cond = _symbolize_condition(term[0])
-        symbols_true = sympy.sympify(term[1]).atoms(sympy.Symbol)
-        symbols_false = sympy.sympify(term[2]).atoms(sympy.Symbol)
-        symbols_set = symbols_true | symbols_false
-        # symbols_set = symbols_cond | symbols_true | symbols_false
+        symbols_cond = _symset_in_condition(term[0])
+        symbols_true = _get_atoms(term[1])
+        symbols_false = _get_atoms(term[2])
+        symbols_set = symbols_cond | symbols_true | symbols_false
         str_symbols_set = {str(symbol) for symbol in symbols_set}
 
     return str_symbols_set
-
-
-CMP_SIGNS = ["==", "!=", ">", "<", ">=", "<=", " and ", " or ", "|", "&&"]
-
-
-def _symbolize_condition(cond: str) -> Set[str]:
-    def _split_by_sign(cmp: str, ans=None):
-        for sign in CMP_SIGNS:
-            pass
-
-    return set(["a", "b"])
 
 
 def str_symbol_name(name: str) -> str:
